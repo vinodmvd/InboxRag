6b771e6f59c695c4f1377d813bcbf60a96fd557c
EC2 Default User <ec2-user@ip-172-31-3-68.ap-south-1.compute.internal>
2025-12-02 07:52:30 +0000

    Pushed changed from AWS EC2

--- query_retriever/logger_config.py
+++ query_retriever/logger_config.py
@@ -1,8 +1,15 @@
 import logging
+import os
+from dotenv import load_dotenv
+
+#Initialize env vars
+load_dotenv()
+level = os.getenv('log_code')
+logger_code= getattr(logging, level)
 
 def initialize_log_config():
     logging.basicConfig(
-        level=logging.INFO, 
+        level=logger_code, 
         format= '%(asctime)s | %(levelname)s | %(name)s -> %(message)s'
         )
     
